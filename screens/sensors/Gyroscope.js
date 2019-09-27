b8581c577f8a548c9a8bde689065e30e97e83802
Gotzsche <pierregotzsche@gmail.com>
2019-09-27 16:57:46 +0200

    commit GYROSCOPE

--- screens/sensors/Gyroscope.js
+++ screens/sensors/Gyroscope.js
@@ -8,13 +8,27 @@ class Gyroscope extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      gyroscopeData: {}
+      gyroscopeData: {},
+      position: {
+        top: 100,
+        left: 100
+      }
     };
   }
 
   componentDidMount() {
     ExpoGyroscope.setUpdateInterval(100);
-    this._gyroscopeSubscription = ExpoGyroscope.addListener(data => this.setState({ gyroscopeData: data }))
+    this._gyroscopeSubscription = ExpoGyroscope.addListener(data => this.setState({ gyroscopeData: data }));
+    setInterval(() => {
+      const {top, left} = this.state.position;
+      const {x, y} = this.state.gyroscopeData;
+      this.setState({
+        position: {
+          top: top + x,
+          left: left + y
+        }
+      })
+    }, 100);
   }
 
   componentWillUnmount() {
@@ -23,10 +37,14 @@ class Gyroscope extends Component {
   }
 
   render() {
+
+    const {top, left} = this.state.position;
+
     return (
       <View>
         <Text>Gyroscope :</Text>
         <SensorItem data={this.state.gyroscopeData}/>
+        <View style={{ position: 'absolute', top: top, left: left, height: 50, width: 50, borderRadius: 50, backgroundColor: 'red' }}></View>
       </View>
     );
   }
